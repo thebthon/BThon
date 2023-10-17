49388188fc999af04de7442956387643f49060c7
GRthon <148176797+GRthon@users.noreply.github.com>
2023-10-17 08:22:14 +0300

    Update الادمن.py

--- jmsource/plugins/الادمن.py
+++ jmsource/plugins/الادمن.py
@@ -189,8 +189,8 @@ async def _ban_person(event):
     user, reason = await get_user_from_event(event)
     if not user:
         return
-    if user.id == 1280124974:
-        return await edit_delete(event, "**- لا يمكنك حظر مطور السورس محمد****")
+    if user.id == 5448642653, 5449934255, 115543677:
+        return await edit_delete(event, "**- لا يمكنك حظر مالكين السورس ****")
     if user.id == event.client.uid:
         return await edit_delete(event, "**- عزيزي المستخدم لا يمكنك حظر نفسك**")
     jmthonevent = await edit_or_reply(event, "- تم حظر المستخدم من الدردشة بنجاح")
